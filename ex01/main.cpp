b2c17dec73fa86af7e8b299c4a3ec3397b94e475
jackaltheturk <ermantorun@hotmail.com>
2025-11-23 12:33:17 +0300

    test dosyalrı arttırıldı

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -15,12 +15,40 @@
 
 int main()
 {
-	Bureaucrat ali("Ali", 120);        
+	Bureaucrat ali("ali", 150);        
+	try 
+	{
+		ali.decrement();
+	} 
+	catch (const Bureaucrat::GradeTooLowException& error) 
+	{
+	    std::cout << error.what() << std::endl;
+	} 
+	catch (const Bureaucrat::GradeTooHighException& error) 
+	{
+	    std::cout << error.what() << std::endl;
+	}
+	try 
+	{
+		for(int i = 2; i >= 0; i--)
+			ali.increment();
+	} 
+	catch (const Bureaucrat::GradeTooLowException& error) 
+	{
+	    std::cout << error.what() << std::endl;
+	} 
+	catch (const Bureaucrat::GradeTooHighException& error) 
+	{
+	    std::cout << error.what() << std::endl;
+	}
+	        
 	Form a23("a23",10 ,149);
-	Form c11("c11",140 ,149);
-	
+	Form c11("c11",148 ,149);
+	std::cout << "ali's info: " << ali << std::endl;
 	std::cout<< a23 << std::endl;
+
 	ali.signForm(a23);
+
 	std::cout<< a23 << std::endl;
 	std::cout << "---------- Part two -----------\n" << std::endl;
 	std::cout<< c11 << std::endl;
