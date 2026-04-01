579738a050f52ae775f451ad8c66115f09c3eaaf
jackaltheturk <ermantorun@hotmail.com>
2026-04-01 16:41:45 +0300

    uzun aradan sonra

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: etorun <etorun@student.42istanbul.com.t    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/21 21:11:30 by etorun            #+#    #+#             */
-/*   Updated: 2025/11/22 19:39:30 by etorun           ###   ########.fr       */
+/*   Updated: 2026/04/01 16:40:49 by etorun           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,31 +14,43 @@
 
 int main()
 {
+	std::cout << "Çok düşük grade(150000) yaratılıyor." << std::endl;
+	try 
+	{
+		Bureaucrat worstOne("Badboy", 150000);
+	} 
+	catch (const std::exception& e) 
+	{
+	    std::cout << e.what() << std::endl;
+	}
+	std::cout << "Ali grade(150) yaratılıyor." << std::endl;
 	Bureaucrat ali("Ali", 150);        
 	try 
 	{
-	   ali.decrement();
+		std::cout << "Ali'nin gradesi düşürülmeye çalışılıyor." << std::endl;
+		ali.decrement();
 	} 
-	catch (const Bureaucrat::GradeTooLowException& error) 
+	catch (const std::exception& e) 
 	{
-	    std::cout << error.what() << std::endl;
+		std::cout << e.what() << std::endl;
 	} 
-	catch (const Bureaucrat::GradeTooHighException& error) 
+	std::cout << "Last status: " << ali << std::endl;
+	std::cout << "Veli grade(2) yaratılıyor." << std::endl;
+	Bureaucrat veli = Bureaucrat("Veli",2);
+	try
 	{
-	    std::cout << error.what() << std::endl;
+		std::cout << "Veli(2) grade yükseltiliyor." << std::endl;
+		veli.increment();
+		std::cout << "Promoted veli says: " << veli << std::endl;
+		std::cout << "Veli'nin gradesi ikici kez yükseltilmeye çalışılıyor." << std::endl;
+		veli.increment();
+		std::cout << "NonPromoted veli says: " << veli << std::endl;
+		//This line of the code will never run. Left intentionally to demostrate.
 	}
-	std::cout << ali << std::endl;
-	Bureaucrat ali2 = Bureaucrat(ali);
-	std::cout << "ali copied,it is ali2" << std::endl; 
-	try{ali2.increment();}
-	catch (const Bureaucrat::GradeTooLowException& error) 
+	catch (const std::exception& e) 
 	{
-	    std::cout << error.what() << std::endl;
-	} 
-	catch (const Bureaucrat::GradeTooHighException& error) 
-	{
-	    std::cout << error.what() << std::endl;
+	    std::cout << e.what() << std::endl;
 	}
-	std::cout << "Promoted ali2 says: " << ali2 << std::endl;
+	std::cout << "Last status: " << veli << std::endl;
 	return 0;
 }
